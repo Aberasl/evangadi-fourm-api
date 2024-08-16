6cc474a380c07ed33a720a647fa11a2ad2c67201
Aberasl <aberagebey80@gmail.com>
2024-08-16 18:53:39 -0500

    config

--- db/config.js
+++ db/config.js
@@ -2,20 +2,13 @@ const mysql2 = require("mysql2");
 require("dotenv").config();
 
 const dbConnection = mysql2.createPool({
-  host: process.env.HOST,
-  port:process.env.PORT,
-
-
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-//   port:5432,
-  connectionLimit: 10,
-  
-  
-}
-
-);
+  host: process.env.HOST || "localhost", // Default to 'localhost' if not set
+  port: parseInt(process.env.PORT, 10) || 3306, // Default to 3306 if not set; ensure PORT is a number
+  user: process.env.USER || "root",
+  password: process.env.PASSWORD || "",
+  database: process.env.DATABASE || "mydatabase",
+  connectionLimit: 10, // Number of connections to create in the pool
+});
 
 // Test the connection
 dbConnection.execute("SELECT 1", (err, result) => {
