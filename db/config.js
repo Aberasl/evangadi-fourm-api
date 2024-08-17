ac6e89a81f89cda8c4af59dbe7ef744aa7b82414
Aberasl <aberagebey80@gmail.com>
2024-08-17 15:45:42 -0500

    lela

--- db/config.js
+++ db/config.js
@@ -1,13 +1,23 @@
 const mysql2 = require("mysql2");
 require("dotenv").config();
 
+// const dbConnection = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT, 10),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   connectionLimit: 10, // Number of connections to create in the pool
+// });
+
 const dbConnection = mysql2.createPool({
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectionLimit: 10, // Number of connections to create in the pool
+  port: process.env.DB_PORT,
+  // port: process.env.PORT,
+  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10),
 });
 
 // Test the connection
