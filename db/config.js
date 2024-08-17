5358377da7cc781e6ea6718611f963a850284190
Aberasl <aberagebey80@gmail.com>
2024-08-17 10:59:09 -0500

    new

--- db/config.js
+++ db/config.js
@@ -2,11 +2,11 @@ const mysql2 = require("mysql2");
 require("dotenv").config();
 
 const dbConnection = mysql2.createPool({
-  host: process.env.HOST,
-  // port: parseInt(process.env.PORT, 10),
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   connectionLimit: 10, // Number of connections to create in the pool
 });
 
